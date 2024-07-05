60391c3d7201aa007dc7d873fca1726f0c194d4d
jkson83 <ugwha@naver.com>
2024-07-05 17:10:44 +0900

    vue-study

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template> 
+  <input type="checkbox" id="todo-item" :checked="isDone" />
+  <label for="todo-item">{{ label }}</label>
+</template>
+
+<script>
+export default {
+    props: {
+        label: { required: true, type: String },
+        done:{ default:false, type:Boolean},
+    },
+    data() {
+        return {
+            isDone: this.done,
+        };
+    }
+}
+</script>
